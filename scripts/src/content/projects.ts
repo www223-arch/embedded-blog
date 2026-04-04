95ddab04d67c037e6474a9c900b883c150395a64
www223-arch <3577454701@qq.com>
2026-04-05 00:35:01 +0800

    部署尝试

--- scripts/src/content/projects.ts
+++
@@ -1,6 +0,0 @@
-import { projectSchema, type ProjectItem } from "./schema";
-
-const projectsRaw = [
-] as const;
-
-export const projectItems: ProjectItem[] = projectsRaw.map((item) => projectSchema.parse(item));
--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import "./style.css";
-import { bootstrap } from "./app/bootstrap";
-
-bootstrap();
